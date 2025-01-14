3829e125309222829248902f08ddc7b2b7792135
Juan Pérez <133648979+JuanPerez07@users.noreply.github.com>
2025-01-14 17:27:53 +0100

    updated with VFH

--- Following_State.py
+++ Following_State.py
@@ -9,10 +9,7 @@ import math
 from smach import State, StateMachine
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
-from std_msgs.msg import String
-
-ANG_IZQ = 30*math.pi/180.0
-ANG_DER = -ANG_IZQ
+from std_msgs.msg import String, Int32
 
 FREEWAY_LINEAR_SPEED = 0.2
 FREEWAY_ANGULAR_SPEED = 0.5
@@ -39,26 +36,31 @@ class FollowingState(State):
         self.personSub = rospy.Subscriber(PERSON_TOPIC, String, self.person_cb )
         self.commandSub = rospy.Subscriber(COMMAND_TOPIC, String, self.command_cb )
         self.scanSub = rospy.Subscriber(SCAN_TOPIC, LaserScan, self.laser_cb)
-        
-
+        self.obsSub = rospy.Subscriber(OBS_TOPIC, String, self.obs_cb)  
+        self.targetSub = rospy.Subscriber(TARGET_TOPIC, Int32, self.target_cb)
+        self.lastTwistSub = rospy.Subscriber('/cmd_vel', Twist, self.last_velocity_cb)
         # attributes
         self.person = False
         self.command = None
-
+        self.target_direction = None
         # freeWay linear: usar profundidad camara o dist laser frontal
         #TODO freeWay angular: usar desplazamiento bbox
         self.speed = { 
             "freeWay": {"linear_speed": FREEWAY_LINEAR_SPEED , "angular_speed": FREEWAY_ANGULAR_SPEED },
             "obstacle": {"linear_speed": 0.0, "angular_speed": 0.0}
         }
-        self.obstacle = False
-        self.frontal_ray = None
+        self.obstacle = None
         self.safety_threshold = SAFETY_DIST # frontal ray safety distance
         # gains to regulate the linear speed and angular speed while following in a freeWay
         self.kp = {"linear": - math.log(SAFETY_DIST) * GAMMA * (FREEWAY_LINEAR_SPEED/10.0) , "angular": 0.9} # speed gains for freeway
         self.freq = FREQ
         self.last_twist = None # last Twist sent to the tb2
-    
+        # laser scan variables
+        self.max_angle = None # max angle fo the laser beam
+        self.angle_inc = None # angle increment of the laser
+        self.laser_readings = None # array of distances read by the scan
+        # close other plot 
+        plt.close('all')        
 
 
         self.Mailman = True
@@ -66,17 +68,28 @@ class FollowingState(State):
         self.comandoUser = commands['Following State']
         self.start_slam()
 
+    def last_velocity_cb(self, msg):
+        self.last_twist = msg    
+    # store only the frontal 180 degrees beam range from right side of the robot to left side 
     def laser_cb(self, msg):
-        index = len(msg.ranges) // 2 # index of the frontal ray
-        self.frontal_ray = msg.ranges[index] # distance frontal ray
-        # linear speed when there is no obstacle based on frontal ray
-        #self.speed['freeWay']['linear_speed'] = self.frontal_ray * self.kp['linear']
-        # centinela para detectar obstaculo
-        obs = False
-        if self.frontal_ray < self.safety_threshold:
-            obs = True
-        self.obstacle = obs 
-
+        reads = []
+        n = len(msg.ranges)
+        for i in range(90, n - 90):
+            reads.append(msg.ranges[i])        			
+        # convert to numpy array
+        self.laser_readings = np.array(reads)
+        self.angle_inc = np.degrees(msg.angle_increment)
+        self.max_angle = np.degrees((msg.angle_max * 3) // 2) # half as we use half of the laser beams
+    # obstacle topic callback
+    def obs_cb(self, msg):
+        if msg.data == "true":
+            self.obstacle = True
+        else:
+            self.obstacle = False
+    # target direction callback
+    def target_cb(self, msg):
+        self.target_direction = msg.data
+        
     def person_cb(self, msg):
         self.person = msg.data == "detected"
         
@@ -131,13 +144,37 @@ class FollowingState(State):
                     cmd.linear.x = 0.0
                     cmd.angular.z = 0.0
             else: # there is obs so we stop
-                cmd.linear.x = 0.0
-                cmd.angular.z = 0.0
-            # regulate the speed to avoid bumps due to lost of the target
+                omega = 0 # angular velocity
+                target = self.target_direction # original target
+                last_dir = 0
+                speed_directions = [] # list of inverse speeds taken to regain the original target       
+                while(self.obstacle):
+                    print("Performing VFH_PLUS")
+                    linear = 0
+                    # compute the initial histogram and plot it
+                    if self.laser_readings is not None:
+                        print("Target (degrees) : ", target)
+                        vfh = VFHPlus(self.laser_readings, self.angle_inc, self.max_angle, last_dir)
+                        vfh.compute_direction(target)
+                        vfh.plot_histogram(vfh.hist)
+                        direct = vfh.getDirection() 
+                        if direct is not None:
+                            last_dir = direct # renew the last direction taken
+                            target = - direct # new target as we moved away from it
+                            print("Selected direction in degrees: ", np.degrees(direct))
+                            omega = - direct * FREQ * 0.5 # angular speed chosen
+                            speed_directions.append(-omega) # append the opposite direction speed
+                        if omega < 0.00001:
+                            linear = FREEWAY_LINEAR_SPEED
+                            omega = 0  
+                    # assign the speeds        
+                    cmd.linear.x = linear
+                    cmd.angular.z = omega
+                    # publish speeds
+                    self.speedPub.publish(cmd)    
 
-            if self.last_twist is None:
-                self.last_twist = cmd
-                print(' velocidad anterior NONE')
+            
+                
             else: # we already have a previous speed so we regulate it
                 
                 if self.last_twist.linear.x < cmd.linear.x and cmd.linear.x < FREEWAY_LINEAR_SPEED: # increment speed
